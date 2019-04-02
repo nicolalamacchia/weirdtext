6cb3aa11955366b462eed10296f5a0b7caffc979
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2019-04-02 18:35:18 +0200

    Update WordListOutput.js

--- src/components/molecules/WordListOutput.js
+++ src/components/molecules/WordListOutput.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Heading from 'components/atoms/Heading'
 import PreWithPlaceholder from 'components/molecules/PreWithPlaceholder'
 
-const WordListInput = ({ title, words }) => (
+const WordListOutput = ({ title, words }) => (
   <React.Fragment>
     <Heading level="4">{title}</Heading>
     <PreWithPlaceholder text={words.join(' ')} />
   </React.Fragment>
 )
 
-export default WordListInput
+export default WordListOutput
