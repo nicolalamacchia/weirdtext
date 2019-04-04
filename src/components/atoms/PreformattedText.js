83d9783aa3f8926f52ce1060038115bd4e10741d
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2019-04-05 00:05:00 +0200

    Update PreformattedText.js

--- src/components/atoms/PreformattedText.js
+++ src/components/atoms/PreformattedText.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components'
 
-const whiteSpace = ({ wrap }) => (wrap ? 'pre-wrap' : 'initial')
-
 const PreformattedText = styled.pre`
   font-family: sans-serif;
   font-size: 1em;
   overflow-x: auto;
   padding: 0.3em 0;
-  white-space: ${whiteSpace};
+  white-space: pre-wrap;
 `
 
 export default PreformattedText
